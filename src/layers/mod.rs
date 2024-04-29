63ef1d2c592224e4d120f4461b1d639198107b87
Cyber Phantom <inam123451@gmail.com>
2024-04-29 16:51:36 +0530

    layers: Add Identifier enum
    Will be used to assign devices a representation on the network

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -1,1 +1,13 @@
 mod physical;
+
+pub(self) enum Identifier {
+    Name(String),
+}
+
+impl ToString for Identifier {
+    fn to_string(&self) -> String {
+        match self {
+            Identifier::Name(name) => name.clone(),
+        }
+    }
+}
